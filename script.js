ecd5dea788e6638bfb2ad0d9bc390303c7f960c4
himanshu yadav <himanshuyadav1740343@gmail.com>
2025-06-13 12:56:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  if (window.scrollY > 50) {  // Adjust scroll distance as needed
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+// someimportantstyles();
+
+function setactive(element) {
+  const activeElement = document.querySelector('.active');
+  if (activeElement) {
+    activeElement.classList.remove('active');
+  }
+  element.classList.add('active');
+  
+}
+window.addEventListener('scroll', () => {
+  const element1 = document.querySelector('.bganimimg');
+
+  if (window.scrollY > 100) {  // Adjust scroll distance as needed
+   element1.children[0].style = `transform:scale(${window.scrollY/300});`
+   console.log(`transform:scale(0${window.scrollY});`);
+  } else {
+   
+  }
+});
+ const video = document.getElementById('heroimage');
+    video.play();
