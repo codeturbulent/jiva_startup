30319108f90200d824082317e54cba33f163a2da
himanshu yadav <cturbulent1740343@gmail.com>
2026-01-02 00:28:16 +0530

    added about us page

---
+++ beta/assets/autoload.js
@@ -0,0 +1,33 @@
+function addheaderfooter() {
+document.getElementsByTagName("header")[0].innerHTML=` <div class="logo">
+    <h4><a href="index.html"> Jiva.</a></h4>
+</div>
+<div class="new">
+    <span> <a href="feedback.html">Become a Jiva. Tester</a></span>
+</div>
+<div class="navi">
+    <ul>
+        <li><a href="">
+                <h2> Pricing </h2>
+            </a></li>
+        <li><a href="">
+                <h2> Contact </h2>
+            </a></li>
+        <li><a href="about.html">
+                <h2> About </h2>
+            </a></li>
+    </ul>
+</div>
+<div class="cta"><a href="">
+        <h2>Download App</h2>
+    </a></div>`
+document.getElementsByTagName("footer")[0].innerHTML=`<div class="contact"> <a
+        href="mailto:biz@jiva.today?subject=Hello%20Jiva.%20Team"> biz@jiva.today</a> <span>•</span> <a
+        href="tel:+91+919081813697 "> +919081813697 </a></div>
+<div class="tags">Read Flexibly <span>•</span> Think Clearly <span>•</span> Stay in Flow </div>`
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'assets/autoload.css?v=' + new Date().getTime();
+    document.head.appendChild(link);
+}
+addheaderfooter()
